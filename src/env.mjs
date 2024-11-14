5275b2418c15ab2f83a1a7cd95f9ff944797aff0
Stefano De Grandis <degrandis.stefano@gmail.com>
2024-11-14 00:54:55 +0000

    vercel-env

--- src/env.mjs
+++ src/env.mjs
@@ -6,6 +6,10 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : process.env.NEXT_PUBLIC_APP_URL
+        ? process.env.NEXT_PUBLIC_APP_URL
+        : "http://localhost:3000",
   },
 })
